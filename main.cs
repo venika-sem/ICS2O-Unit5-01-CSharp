76629dcc3cb24aef2336e535377cb9d8640e52fb
Venika Sem <99354917+venika-sem@users.noreply.github.com>
2022-05-04 16:49:10 +0000

    changed main.cs

--- main.cs
+++ main.cs
@@ -1,7 +1,38 @@
+// Created by: Venika Sem
+// Created on: May 2022
+//
+// This program guesses a number between 1 and 6 
+
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // This program guesses a number between 1 and 6 
+        int guess;
+        Random randomNumber = new Random();
+        int generatedNumber = randomNumber.Next(1, 6);
+
+        // input
+        Console.WriteLine("This program guesses a number between 1-6");
+
+        Console.WriteLine("");
+        Console.Write("Input a number between 1-6: ");
+        guess = Convert.ToInt32(Console.ReadLine());
+
+        // process and output
+        Console.WriteLine("");
+        if (guess == generatedNumber)
+        {
+            Console.WriteLine("Bravo, you are correct!");
+        }
+
+        if (guess != generatedNumber)
+        {
+            Console.WriteLine("Sorry, you are incorrect!");
+        }
+
+        Console.WriteLine("\nDone.");
+    }
 }
